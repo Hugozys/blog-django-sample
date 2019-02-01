eb5479149c1e7372ab89a63bd3dae26f636ad03b
Hugo <yz395@duke.edu>
2019-02-01 18:38:36 -0500

    change runserver.sh to enable development demo

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sed -i /code/blog/settings/__init__.py -e "s:from \.production:#from \.production:"
+sed -i /code/blog/settings/__init__.py -e "s:#from \.development:from .development:"
+
 res= "1"
 while [ "$res" != "0" ]
 do
