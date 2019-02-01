fceb3eb38f67e4a898bf248d7b6e290de837a876
Hugo <yz395@duke.edu>
2019-02-01 18:06:31 -0500

    remove redundant models

--- authe/models.py
+++ authe/models.py
@@ -5,26 +5,4 @@ from django.contrib.auth.models import User
 
 
 
-class Profile(models.Model):
-    MALE = 'M'
-    FEMALE = 'F'
-    SECRET = 'S'
-    GENDER_CHOICES = (
-        (MALE, 'Male'),
-        (FEMALE, 'Female'),
-        (SECRET,'Secret')
-    )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    gender = models.CharField(max_length=1,
-                              choices=GENDER_CHOICES,
-                              default=SECRET,
-    )
-    facebook = models.CharField(max_length=255,blank=True)
-    instagram = models.CharField(max_length=255,blank=True)
-    wechat = models.CharField(max_length=255,blank=True)
-    website =models.CharField(max_length=255,blank=True)
-    bio = models.CharField(max_length=5000, blank=True)
-    live_place = models.CharField(max_length=255,blank=True)
-    pass
 
