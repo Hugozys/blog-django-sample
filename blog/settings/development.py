e29802ac0d493793ff7b91cc6423f5adf89a7217
Hugo <yz395@duke.edu>
2019-02-01 19:00:50 -0500

    generalize development.py:

--- blog/settings/development.py
+++ blog/settings/development.py
@@ -1,8 +1,10 @@
 from .base import *
+import socket
+import getpass
 SECRET_KEY = '47fn)_(6_r5h^$(_g%=4ft+%rvppnafr*6*aj=usqvokj6kv-m'
 DEBUG = True
 
-ALLOWED_HOSTS = ['vcm-7520.vm.duke.edu','67.159.94.90']
+ALLOWED_HOSTS = [socket.gethostname()]
 
 
 DATABASES = {
@@ -19,7 +21,7 @@ STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.duke.edu'
-DEFAULT_FROM_EMAIL = 'yz395@vcm-7520.vm.duke.edu'
+DEFAULT_FROM_EMAIL = getpass.getuser() + "@" + socket.gethostname()
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
